4a2f7153ea5e123cd548ccc16ee9d2c81da01cd8
mndowne <mndowne@gmail.com>
2017-01-12 07:56:05 -0600

    node app.js command is working and npm start script using webpack

--- app.js
+++ app.js
@@ -2,10 +2,13 @@ var express = require('express');
 var app = express();
 
 app.set('port',(process.env.PORT || 3000));
-//app.use(express.static(__dirname));
+app.use(express.static(__dirname));
+app.use(express.static(__dirname + '/src/'));
+app.use(express.static(__dirname + '/dist/'));
+app.use(express.static(__dirname + '/dist/app/'));
 
 app.get('/', function(req,res){
-  res.render(__dirname + '/index.html');
+  res.render(__dirname + '/src/nodeJSindex.html');
 });
 
 app.listen(app.get('port'), function(){
